a5f1b51760f54490f12d544d19f3485570d23978
OctavioCaminotti <octaviocaminotti123@gmail.com>
2026-04-30 08:48:47 -0300

    feat: optimizacion y creacion de componentes

--- src/components/Biblioteca/Biblioteca.jsx
+++ src/components/Biblioteca/Biblioteca.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import GameCard from '../GameCard/GameCard'
 
 const Biblioteca = ({ games, setGames, biblioteca }) => {
   return (
-    <div>
+    <div >
       <h1>Mi Biblioteca</h1>
 
       {biblioteca.length === 0 ? (
@@ -11,7 +12,7 @@ const Biblioteca = ({ games, setGames, biblioteca }) => {
         <ul>
           {biblioteca.map((game, index) => (
             <li key={index}>
-              <h4>{game.title} </h4><img src={game.poster} alt={game.title} style={{width: '200px', height: '300px', objectFit: 'cover'}} />
+              <h4>{game.title} </h4><img src={game.poster} alt={game.title} style={{ width: '200px', height: '300px', objectFit: 'cover' }} />
             </li>
           ))}
         </ul>
@@ -21,3 +22,7 @@ const Biblioteca = ({ games, setGames, biblioteca }) => {
 }
 
 export default Biblioteca
+
+/*
+importar gameCard para mostrar los juegos en biblioteca
+*/
